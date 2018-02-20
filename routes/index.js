e0510fd97bee976b2985ae2c753a1feaf4e618ef
Justin Rice <jsrice7391@gmail.com>
2018-02-20 11:16:57 -0500

    Working application with all the build scripts

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 const path = require("path");
 const router = require("express").Router();
-// Takes in all of the routes that will be established on the API side.
 const apiRoutes = require("./api");
 
-
-// Establish the API routes
+// API Routes
 router.use("/api", apiRoutes);
 
-// If no route is specified within the API routes, send them to the REACT folder.
-router.use(function(req,res, next){
-    res.sendFile(path.join(__dirname, "../client/public/index.html"))
-})
-
-
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -3,32 +3,22 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const logger = require("morgan");
-
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
-app.use(express.static("client"));
+app.use(express.static("client/build"));
 // Add routes, both API and view
-app.use(routes.router);
-
-app.use(logger("dev"));
+app.use(routes);
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactscraper",
-  {
-    useMongoClient: true
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
